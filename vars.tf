f5d095f768cad12d17b0c2f03191d0f88b4e2807
jevargascr <jeffrey.vargas@gmail.com>
2022-12-16 12:27:27 -0600

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,2 +1,7 @@
 variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaaqcgo3o3kzrbfsnwxtyab74yvfg46isooezgwsvjmczuutl4ucwyq" }
 variable region { default = "us-ashburn-1" }
+variable "create_service_gateway" {
+  description = "whether to create a service gateway. If set to true, creates a service gateway."
+  default     = false
+  type        = bool
+}
